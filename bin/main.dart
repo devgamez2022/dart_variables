8d215411cabc6217e3269d9a48d24e4e381240f0
devgamez2022 <winmanuel09@gmail.com>
2022-05-23 14:27:32 -0600

    hola

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  String mensaje = 'Bienvenidos a Dart';
+  int a, b, suma;
+  a = 30;
+  b = 55;
+
+  //suma = a + b;
+  suma = 0;
+
+  print('Programador x $mensaje');
+
+  print('La suma de $a + $b = $suma');
+
+  print('La suma de $a + $b = ${a + b}');
+}
